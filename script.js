1a56f85a9f87cf26bbc811c05172fbed7a073fc4
cn-David Ryan <cn-dryan@alertinnovation.com>
2024-01-05 16:23:46 -0600

    added counter to DOM from js file

--- script.js
+++ script.js
@@ -29,16 +29,7 @@ function handleTickInit(tick) {
         tick.value = value;
     };
 
-    counter.onended = function () {
-        // redirect, uncomment the next line
-        // window.location = 'my-location.html'
 
-        // hide counter, uncomment the next line
-        // tick.root.style.display = 'none';
-
-        // show message, uncomment the next line
-        // document.querySelector('.tick-onended-message').style.display = '';
-    };
 }
 
 // Fetch and parse JSON data
@@ -51,7 +42,73 @@ fetchLaunchData(apiUrl)
 
             // Now you can use the variable t0Value as needed
             console.log('Value of "t0":', nextLaunch);
+
+
+            var element = document.querySelector('#myCounter');
+            Tick.DOM.create(element, {
+                //value: 1000,
+
+                view:
+                // definition for top level tick element
+                {
+                    children: [
+                        // presenter object
+                        {
+                            root: 'div',
+                            repeater: true,
+                            layout: 'horizontal center fit',
+                            'data-transform': 'preset(d, h, m, s) -> delay',
+                            
+                            //transform: 'preset(d, h, m, s)',
+                            children: [
+                                // presenter object
+                                {
+                                    root: 'div',
+                                    class: 'tick-group',
+                                    children: [
+                                        // presenter object
+                                        {
+                                            root: 'div',
+                                            key: 'value',
+                                            repeater: true,
+                                            transform: 'pad(00) -> split -> delay',
+                                            children: [
+                                                // presenter object
+                                                {
+                                                    root: 'span',
+                                                    view: 'flip'
+                                                }
+                                            ]
+                                        },
+                                        // presenter object
+                                        {
+                                            root: 'span',
+                                            key: 'label',
+                                            view: 'text',
+                                            class: 'tick-label'
+                                        }
+                                    ]
+                                },
+                            ]
+                        }
+                    ]
+                },
+
+                didInit: function (tick) {
+                    // create the countdown counter
+                    var counter = Tick.count.down(nextLaunch);
+
+                    counter.onupdate = function (value) {
+                        tick.value = value;
+                    };
+
+                }
+            });
+
         } else {
             console.log('No launch data available.');
         }
     });
+
+
+
